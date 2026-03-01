c575c36d5e4bd6f8b68686b9abee092c6d46ad2c
syuririk <songjeff0@g.skku.edu>
2026-03-01 09:09:23 +0000

    0.1.20

--- src/getData/fisis/endpoints/getData.py
+++ src/getData/fisis/endpoints/getData.py
@@ -1,4 +1,5 @@
 from getData.fisis.services.statisticsInfoSearch import statisticsInfoSearch
+from getData.fisis.utils.convertData import c_statInfo_to_df
 
 import pandas as pd
 
@@ -10,10 +11,12 @@ def getData(codes, start_date, end_date):
     accountCd = code[2] 
     term = code[3] 
     data = statisticsInfoSearch(financeCd= financeCd, 
-                              listNo= listNo, 
-                              accountCd= accountCd, 
-                              term= term, 
-                              startBaseMm= start_date, 
-                              endBaseMm= end_date)
-    dfs.appned(data)
+                                listNo= listNo, 
+                                accountCd= accountCd, 
+                                term= term, 
+                                startBaseMm= start_date, 
+                                endBaseMm= end_date)
+    data = c_statInfo_to_df(data)
+    dfs.append(data)
+
   pd.concat(dfs, axis=1)
