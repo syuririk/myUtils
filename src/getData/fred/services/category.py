69effe5466e4ce2859e117cb80e618d9c56583aa
syuririk <songjeff0@g.skku.edu>
2026-03-01 10:45:28 +0000

    0.2.3

--- src/getData/fred/services/category.py
+++ src/getData/fred/services/category.py
@@ -3,12 +3,5 @@ from .children import children
 
 def category(category_id):
     url = f"https://api.stlouisfed.org/fred/category?category_id={category_id}&api_key={API.get_api_key()}&file_type=json"
-    result = getRequest(url)
-
-    try:
-        result = result.get('categories')[0]
-        result['children'] = children(category_id)
-    except:
-        result = data
-
-    return result
+    data = getRequest(url)
+    return data
--- src/getData/fred/services/series.py
+++ src/getData/fred/services/series.py
@@ -2,12 +2,5 @@ from .base import *
 
 def series(id, start_date, end_date):
     url = f"https://api.stlouisfed.org/fred/series?series_id={id}&api_key={API.get_api_key()}&file_type=json&realtime_start={start_date}&realtime_end={end_date}"
-    params = {
-        "series_id" : id,
-        "api_key" : self.api_key,
-        "file_type" : "json",
-        "realtime_start" : start_date,
-        "realtime_end" : end_date
-    }
-    data = self.request(url, params=params)
-    return data.get('seriess')
+    data = getRequest(url).get('seriess')
+    return data
