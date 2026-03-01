f4219ef248d79029e3902c3616ad711d91b22e03
syuririk <songjeff0@g.skku.edu>
2026-03-01 11:11:11 +0000

    0.2.9

--- src/getData/fred/__init__.py
+++ src/getData/fred/__init__.py
@@ -1,10 +1,11 @@
 from .endpoints.searchCategory import searchCategory
 from .endpoints.searchTags import searchTags
 from .endpoints.setApiKey import setApiKey
+from .endpoints.searchSeries import searchSeries
 
 
 from .services.category import category
 
 __all__ = [
-    'searchCategory', 'setApiKey', 'searchTags'
+    'searchCategory', 'setApiKey', 'searchTags', 'searchSeries'
 ]
