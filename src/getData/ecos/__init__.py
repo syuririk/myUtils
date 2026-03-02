d00a9bbc4e9939cf0c29ce414a8897bfe7f4bad4
syuririk <songjeff0@g.skku.edu>
2026-03-02 04:20:27 +0000

    0.3.3

--- src/getData/ecos/__init__.py
+++ src/getData/ecos/__init__.py
@@ -7,8 +7,12 @@ from .services.statisticSearch import statisticSearch
 from .services.statisticTableList import statisticTableList
 from .services.statisticWord import statisticWord
 
+from getData.fisis.utils.api import API
+
+
 __all__ = [
     'setApiKey',
 
     'keyStatisticList', 'statisticItemList', 'statisticMeta', 'statisticSearch', 'statisticTableList', 'statisticWord'
+    'API'
 ]
