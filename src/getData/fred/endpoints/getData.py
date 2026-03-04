f8cd593e12ad776e7ce0c1f821106709848691e1
syuririk <songjeff0@g.skku.edu>
2026-03-04 11:27:46 +0000

    0.5.13

--- src/getData/fred/endpoints/getData.py
+++ src/getData/fred/endpoints/getData.py
@@ -4,9 +4,11 @@ import pandas as pd
 
 def getData(codes, start_date, end_date):
     dfs = []
+    start_var = f"{start_date[:4]}-{start_date[4:6]}-{start_date[6:]}"
+    end_date = f"{end_date[:4]}-{end_date[4:6]}-{end_date[6:]}"
+    
     for code in codes:
-        start_var = f"{start_date[:4]}-{start_date[4:6]}-{start_date[6:]}"
-        end_date = f"{end_date[:4]}-{end_date[4:6]}-{end_date[6:]}"
+
         data = seriesObservations(code, start_var, end_date)
         df = pd.DataFrame(data)
 
